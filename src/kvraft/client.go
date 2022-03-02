78466af6b561a9c3423c1adc2ecd0ee55c01cf9f
Charles Peng <charlespeng1998@outlook.com>
2022-03-02 17:23:42 +0800

    Feat: Implemented clerk sending RPC to server

--- src/kvraft/client.go
+++ src/kvraft/client.go
@@ -1,13 +1,17 @@
 package kvraft
 
-import "6.824/labrpc"
-import "crypto/rand"
-import "math/big"
+import (
+	"crypto/rand"
+	"log"
+	"math/big"
 
+	"6.824/labrpc"
+)
 
 type Clerk struct {
-	servers []*labrpc.ClientEnd
-	// You will have to modify this struct.
+	servers      []*labrpc.ClientEnd
+	operation_id int
+	last_leader  int
 }
 
 func nrand() int64 {
@@ -20,7 +24,9 @@ func nrand() int64 {
 func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.servers = servers
-	// You'll have to add code here.
+	ck.operation_id = 0
+	ck.last_leader = 0
+
 	return ck
 }
 
@@ -37,9 +43,31 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 // arguments. and reply must be passed as a pointer.
 //
 func (ck *Clerk) Get(key string) string {
+	args := GetArgs{ID: ck.operation_id, Key: key}
+	reply := GetReply{}
 
-	// You will have to modify this function.
-	return ""
+	possible_leader := ck.last_leader
+	ret := ""
+
+	for {
+		log.Printf("Clerk is sending Get RPC to server %v, Key = %v, operation ID = %v...",
+			possible_leader, key, ck.operation_id)
+		ok := ck.servers[possible_leader].Call("KVServer.Get", &args, &reply)
+		if ok && reply.Success {
+			log.Printf("Clerk succeeds in calling Get to server %v, Key = %v, Value = %v, operation ID = %v...",
+				possible_leader, key, reply.Value, ck.operation_id)
+			ret = reply.Value
+			break
+		} else {
+			log.Printf("Clerk fails in calling Get to server %v, Key = %v, operation ID = %v! Resending...",
+				possible_leader, key, ck.operation_id)
+			possible_leader = (possible_leader + 1) % len(ck.servers)
+		}
+	}
+
+	ck.operation_id += 1
+	ck.last_leader = possible_leader
+	return ret
 }
 
 //
@@ -53,7 +81,33 @@ func (ck *Clerk) Get(key string) string {
 // arguments. and reply must be passed as a pointer.
 //
 func (ck *Clerk) PutAppend(key string, value string, op string) {
-	// You will have to modify this function.
+	args := PutAppendArgs{ID: ck.operation_id, Key: key, Value: value}
+	if op == "Put" {
+		args.Type = 0
+	} else if op == "Append" {
+		args.Type = 1
+	}
+	reply := PutAppendReply{}
+
+	possible_leader := ck.last_leader
+
+	for {
+		log.Printf("Clerk is sending PutAppend RPC to server %v, Type = %v, Key = %v, Value = %v, operation ID = %v...",
+			possible_leader, args.Type, key, value, ck.operation_id)
+		ok := ck.servers[possible_leader].Call("KVServer.PutAppend", &args, &reply)
+		if ok && reply.Success {
+			log.Printf("Clerk succeeds in calling PutAppend RPC to server %v, Type = %v, Key = %v, Value = %v, operation ID = %v...",
+				possible_leader, args.Type, key, value, ck.operation_id)
+			break
+		} else {
+			log.Printf("Clerk fails in calling PutAppend RPC to server %v, Type = %v, Key = %v, Value = %v, operation ID = %v! Resending...",
+				possible_leader, args.Type, key, value, ck.operation_id)
+			possible_leader = (possible_leader + 1) % len(ck.servers)
+		}
+	}
+
+	ck.operation_id += 1
+	ck.last_leader = possible_leader
 }
 
 func (ck *Clerk) Put(key string, value string) {
